e6a092604dbb490dd245202f6ac2079b9b52b791
gelauixx <22104625@usc.edu.ph>
2023-12-17 13:44:41 +0800

    Edited check_availability

--- php/check_availability.php
+++ php/check_availability.php
@@ -3,19 +3,22 @@ include 'connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $appointment_date = $_POST['appointmentDate'];
-    $category = $_POST['category'];
+    // $category = $_POST['category'];
 
 
     $response = [];
 
-    $sqlCountAppointments = "SELECT COUNT(*) AS appointmentCount FROM appointments WHERE day = '$appointment_date' AND category = '$category'";
+    $sqlCountAppointments = "SELECT COUNT(*) AS appointmentCount FROM appointments WHERE day = '$appointment_date'";
     $resultCount = $con->query($sqlCountAppointments);
+
+    // $sqlCountAppointments = "SELECT COUNT(*) AS appointmentCount FROM appointments WHERE day = '$appointment_date' AND category = '$category'";
+    // $resultCount = $con->query($sqlCountAppointments);
 
     if ($resultCount && $rowCount = $resultCount->fetch_assoc()) {
         $dailyLimit = 10;
 
         if ($rowCount['appointmentCount'] >= $dailyLimit) {
-            echo json_encode(['success' => false, 'message' => 'Sorry, our appointments for this department with this date are all booked. Kindly refer to the table and select an alternative date. Thank you!']);
+            echo json_encode(['success' => false, 'message' => 'Sorry, our appointments for this date are all booked. Kindly refer to the table and select an alternative date. Thank you!']);
             exit;
         } else {
             echo json_encode(['success' => true]);
